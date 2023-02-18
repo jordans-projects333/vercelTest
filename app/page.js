d158a5de105fc6d4b7c1c7c25330f36caee6877e
Jordan <jordanroberts333@icloud.com>
2023-02-18 11:25:45 +0000

    hmmmmmmmm?

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 
 
 export default async function Home() {
-  // const data = await fetch(`${process.env.VERCEL_URL}/api/hello`)
-  // const data = await fetch(`https://${VERCEL_URL}/api/hello`)
-  const data = await fetch(`https://vercel-test-b7zs-9bae9zelw-jordans-projects333.vercel.app/api/hello`)
-  // const data2 = await fetch(`https://${process.env.VERCEL_URL}/api/hello`)  
+  
+  // const data = await fetch(`https://vercel-test-b7zs-9bae9zelw-jordans-projects333.vercel.app/api/hello`)
+  const data = await fetch(`https://${process.env.VERCEL_URL}/api/hello`)  
   const json = await data.json()
   return (
     <main>
