614d6760a2ddd69c1e8dfc592a66d41bd6b9e2f7
Jordan <jordanroberts333@icloud.com>
2023-02-18 01:51:26 +0000

    fff

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default async function Home() {
   const {name} = await fetch(`https://${process.env.VERCEL_URL}/api/hello`)
   return (
     <main>
-      <h3>{name}</h3>
+      <h3>{name == 'undefined' ? 'boo' : 'yay'}</h3>
     </main>
   )
 }
