e2d1043fa3a18dfca1d0362e13b4342f6329108f
Jordan <jordanroberts333@icloud.com>
2023-02-18 10:30:40 +0000

    uuugh

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 
 
 export default async function Home() {
-  // const name = await fetch(`https://${process.env.VERCEL_URL}/api/hello`).then(res => res.json())
-  // const {name} = await fetch(`${process.env.VERCEL_URL}/api/hello`)
-  const data = await fetch(`${process.env.VERCEL_URL}/api/hello`)
+  // const data = await fetch(`${process.env.VERCEL_URL}/api/hello`)
+  const data = await fetch(`https://${process.env.VERCEL_URL}/api/hello`)
   const json = await data.json()
   return (
     <main>
