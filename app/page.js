6bfd2c748913a8d191933b8bd1ab4f916745d09d
Jordan <jordanroberts333@icloud.com>
2023-02-18 11:30:55 +0000

    hail mary

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@
 export default async function Home() {
   
   // const data = await fetch(`https://vercel-test-b7zs-9bae9zelw-jordans-projects333.vercel.app/api/hello`)
-  const data = await fetch(`https://${process.env.VERCEL_URL}/api/hello`)  
+  // const data = await fetch(`https://${process.env.VERCEL_URL}/api/hello`)
+  const data = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/hello`)  
   const json = await data.json()
   return (
     <main>
       <h3>{process.env.VERCEL_URL}</h3>
       <h3>{json.name}</h3>
+      <h3>hail mary</h3>
     </main>
   )
 }
